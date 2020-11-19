67124e743fc03046b6a865d0a291a4370a9d2fe3
cclin <cclinet@163.com>
2020-11-19 16:17:38 +0800

    rm test

--- test_pytorch.py
+++
@@ -1,6 +0,0 @@
-import torch
-is_gpu = torch.cuda.is_available()
-if is_gpu:
-    print(0)
-else:
-    print(-1)
--- test_tensorflow.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-is_gpu = tf.test.is_gpu_available()
-if is_gpu:
-    print(0)
-else:
-    print(-1)
